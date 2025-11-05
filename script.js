1debebdffca3482da1a2faf4bbcff862b5e5ed4f
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-11-05 10:05:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Add method to Car prototype
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+  // Call Car constructor to inherit properties
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// Inherit Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset constructor reference
+SportsCar.prototype.constructor = SportsCar;
+
+// Add method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
